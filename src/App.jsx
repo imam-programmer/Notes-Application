879ac6458b2f4ce323d5a1fdef0c121db89fcd4a
imam-programmer <imamprogrammer09@gmail.com>
2026-05-29 08:51:30 +0600

    half done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+  const [input, setinput] = useState("")
+  const [textarea, settextarea] = useState("")
+  const [content, setcontent] = useState([])
   return (
     <>
-<form action="" className='flex flex-col w-1/3 gap-5'>
-  <input type="text" placeholder='Enter note heading' className='text-amber-50 border p-3 rounded-[10px]' />
-  <textarea name="" id="" placeholder='Enter details' className='text-amber-50 border pl-3 pb-5 pt-1 rounded-[10px]'></textarea>
+<form action="" className='flex flex-col w-1/3 gap-5' onSubmit={(e)=>{
+  e.preventDefault()
+  const newContent=[...content]
+ newContent.push({input,textarea})
+ setinput("")
+ settextarea("")
+ setcontent(newContent)
+ console.log(content)
+
+}}>
+  <input type="text" placeholder='Enter note heading' className='input' value={input} onChange={(e)=>{
+    setinput(e.target.value)
+  }}/>
+  <textarea name="" id="" placeholder='Enter details' className='textarea' value={textarea} onChange={(e)=>{
+    settextarea(e.target.value)
+  }}></textarea>
   <button className='button'>Add</button>
 </form>
+<div>
+  
+</div>
+{
+  content.map((item,idx)=>{
+return(<div key={idx} className='bg-amber-400 flex'>
+  <div>
+  <h1 className='text-white' >{item.input}</h1>
+  <p className='text-white'>{item.textarea}</p>
+  </div>
+
+</div>
+)
+  })
+}
     </>
   )
 }
